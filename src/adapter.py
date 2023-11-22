1a6a6233f015fabc2323d5d9a26955a716132d83
Luis Henrique <henriqueluis3104@hotmail.com>
2023-11-22 18:38:50 -0300

    feat: add read yaml file function

--- src/adapter.py
+++ src/adapter.py
@@ -1,5 +1,8 @@
+import yaml
 from fastapi import FastAPI
 
 
 class SAM(FastAPI):
-    ...
+    def read_yml_file(self, path="template.yaml"):
+        with open(path) as file:
+            return yaml.safe_load(file)
