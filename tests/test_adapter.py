890991d66744eb98f7cb67952eaed2f2e11ed0dc
Luis Henrique <henriqueluis3104@hotmail.com>
2023-11-22 18:39:44 -0300

    test: add read yaml file test

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from fastapi import FastAPI
@@ -9,3 +10,14 @@ class TestSAMAdapter(unittest.TestCase):
     def test_sam_instance_of_fastapi(self):
         sam = SAM()
         self.assertIsInstance(sam, FastAPI)
+
+    def test_read_yml_file(self):
+        sam = SAM()
+
+        current_path = os.path.dirname(os.path.abspath(__file__))
+
+        file_path = os.path.join(current_path, "template.yaml")
+
+        file = sam.read_yml_file(file_path)
+
+        self.assertIsNotNone(file)
