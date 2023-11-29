a6da2d3581c8d5ed08f7b760551250e66f74a2a1
Gabriel Figueiredo <53541827+gabriel-f-santos@users.noreply.github.com>
2023-11-29 12:21:25 -0300

    feat: add function import_handler (#21)
    * feat: add function get_handler
    
    * refactor: fix pull request, move import_handler to module function and improve tests
    
    * refactor: move import handler to new class unittest
    
    * refactor: change name test to TestImportHandler
    
    * refactor: change module_name variable

---
+++ tests/fixtures/handlers/lambda_handler.py
@@ -0,0 +1,12 @@
+import json
+
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": "hello world",
+            }
+        ),
+    }
