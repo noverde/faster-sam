4bb6c5ef0602a325a38bbfb39b23d1811a54cdcd
Luis Henrique <henriqueluis3104@hotmail.com>
2023-11-30 15:05:53 -0300

    refactor: update sam class in adapter module

--- src/adapter.py
+++ src/adapter.py
@@ -1,11 +1,6 @@
-from typing import Optional
-
 from fastapi import FastAPI
 
-from cloudformation import Template
 
-
-class SAM(FastAPI):
-    def __init__(self, template: Optional[str] = None, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        self._cloudformation = Template(template)
+class SAM:
+    def __init__(self, app: FastAPI) -> None:
+        self.app = app
--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -1,27 +1,14 @@
 import unittest
-from pathlib import Path
+
+from fastapi import FastAPI
 
 from adapter import SAM
-from cloudformation import CFTemplateNotFound
 
 
 class TestSAMAdapter(unittest.TestCase):
     def setUp(self) -> None:
         self.path_templates = "tests/fixtures/templates"
 
-    def test_initialization_with_template(self):
-        sam = SAM(f"{self.path_templates}/example1.yml")
+    def test_initialization(self):
+        sam = SAM(FastAPI())
         self.assertIsInstance(sam, SAM)
-
-    def test_initialization_without_template(self):
-        symlink = Path("template.yml")
-        symlink.symlink_to("tests/fixtures/templates/example1.yml")
-
-        sam = SAM()
-        symlink.unlink()
-
-        self.assertIsInstance(sam, SAM)
-
-    def test_initialization_without_template_exception(self):
-        with self.assertRaises(CFTemplateNotFound):
-            SAM()
