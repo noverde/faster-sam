bc1d4685eb1707c06ae77517c633a337e196f9f6
Luis Henrique <henriqueluis3104@hotmail.com>
2023-11-30 15:36:31 -0300

    refactor: remove unnecessaray function

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -6,9 +6,6 @@ from adapter import SAM
 
 
 class TestSAMAdapter(unittest.TestCase):
-    def setUp(self) -> None:
-        self.path_templates = "tests/fixtures/templates"
-
     def test_initialization(self):
         sam = SAM(FastAPI())
         self.assertIsInstance(sam, SAM)
