a0f05963dd0777801cd899bb5362a4bed444d50a
Luis Henrique <henriqueluis3104@hotmail.com>
2023-11-30 15:34:09 -0300

    feat: add template path param

--- src/adapter.py
+++ src/adapter.py
@@ -1,6 +1,8 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 
 class SAM:
-    def __init__(self, app: FastAPI) -> None:
+    def __init__(self, app: FastAPI, template_path: Optional[str] = None) -> None:
         self.app = app
