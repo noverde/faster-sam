591e640dfd5ef6434a0d94de435314540330d2ce
Juliano Fernandes <julianofernandes@gmail.com>
2023-11-30 17:35:36 -0300

    feat: build routes metadata when initializing sam adapter (#41)

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -3,9 +3,17 @@ import unittest
 from fastapi import FastAPI
 
 from adapter import SAM
+from cloudformation import CloudformationTemplate
 
 
-class TestSAMAdapter(unittest.TestCase):
+class TestSAM(unittest.TestCase):
     def test_initialization(self):
-        sam = SAM(FastAPI())
+        app = FastAPI()
+        template_path = "tests/fixtures/templates/example2.yml"
+        sam = SAM(app, template_path)
+
         self.assertIsInstance(sam, SAM)
+        self.assertEqual(id(app), id(sam.app))
+        self.assertIsInstance(sam.template, CloudformationTemplate)
+        self.assertIsInstance(sam.routes, dict)
+        self.assertGreater(len(sam.routes), 0)
