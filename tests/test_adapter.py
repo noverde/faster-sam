24661029fcc9b4205fa5201ffe69d8e2f7ea636c
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2023-11-30 19:26:44 -0300

    fix: fix key error in function events loop

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -8,12 +8,19 @@ from cloudformation import CloudformationTemplate
 
 class TestSAM(unittest.TestCase):
     def test_initialization(self):
-        app = FastAPI()
-        template_path = "tests/fixtures/templates/example2.yml"
-        sam = SAM(app, template_path)
+        templates = [
+            {"template_path": "tests/fixtures/templates/example1.yml", "expected_route_count": 1},
+            {"template_path": "tests/fixtures/templates/example2.yml", "expected_route_count": 2},
+            {"template_path": "tests/fixtures/templates/example3.yml", "expected_route_count": 2},
+        ]
 
-        self.assertIsInstance(sam, SAM)
-        self.assertEqual(id(app), id(sam.app))
-        self.assertIsInstance(sam.template, CloudformationTemplate)
-        self.assertIsInstance(sam.routes, dict)
-        self.assertGreater(len(sam.routes), 0)
+        for template in templates:
+            with self.subTest(template_path=template["template_path"]):
+                app = FastAPI()
+                sam = SAM(app, template["template_path"])
+
+                self.assertIsInstance(sam, SAM)
+                self.assertEqual(id(app), id(sam.app))
+                self.assertIsInstance(sam.template, CloudformationTemplate)
+                self.assertIsInstance(sam.routes, dict)
+                self.assertGreaterEqual(len(sam.routes), template["expected_route_count"])
