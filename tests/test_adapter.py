cccb11006dab43236325bd6d0683961385bbfe72
Luis Henrique <henriqueluis3104@hotmail.com>
2023-12-01 16:10:27 -0300

    feat: add lambda handler

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -9,9 +9,18 @@ from cloudformation import CloudformationTemplate
 class TestSAM(unittest.TestCase):
     def test_initialization(self):
         templates = [
-            {"template_path": "tests/fixtures/templates/example1.yml", "expected_route_count": 1},
-            {"template_path": "tests/fixtures/templates/example2.yml", "expected_route_count": 2},
-            {"template_path": "tests/fixtures/templates/example3.yml", "expected_route_count": 2},
+            {
+                "template_path": "tests/fixtures/templates/example1.yml",
+                "expected_route_count": 1,
+            },
+            {
+                "template_path": "tests/fixtures/templates/example2.yml",
+                "expected_route_count": 2,
+            },
+            {
+                "template_path": "tests/fixtures/templates/example3.yml",
+                "expected_route_count": 2,
+            },
         ]
 
         for template in templates:
@@ -24,3 +33,28 @@ class TestSAM(unittest.TestCase):
                 self.assertIsInstance(sam.template, CloudformationTemplate)
                 self.assertIsInstance(sam.routes, dict)
                 self.assertGreaterEqual(len(sam.routes), template["expected_route_count"])
+
+    def test_lambda_handler(self):
+        app = FastAPI()
+        sam = SAM(app, "tests/fixtures/templates/example1.yml")
+
+        resources = [
+            {
+                "Properties": {
+                    "CodeUri": "hello_world",
+                    "Handler": "app.lambda_handler",
+                },
+            },
+            {
+                "Properties": {
+                    "CodeUri": "hello_world/",
+                    "Handler": "app.lambda_handler",
+                },
+            },
+        ]
+
+        for resource in resources:
+            with self.subTest():
+                handler_path = sam.lambda_handler(resource["Properties"])
+
+                self.assertEqual(handler_path, "hello_world.app.lambda_handler")
