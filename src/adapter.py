0dca5b1dcf325164f2714470e248f047534d2b9c
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2023-12-05 14:40:33 -0300

    feat: check if function its already mapped

--- src/adapter.py
+++ src/adapter.py
@@ -53,8 +53,8 @@ class SAM:
                     self.routes[id].setdefault(path, {}).update(endpoint)
 
     def lambda_mapper(self):
-        for function in self.template.functions.values():
-            if "Events" not in function["Properties"]:
+        for arn, function in self.template.functions.items():
+            if "Events" not in function["Properties"] or arn in self._mapped_functions:
                 continue
 
             handler_path = self.lambda_handler(function["Properties"])
