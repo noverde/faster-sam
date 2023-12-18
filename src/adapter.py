c1eefe3f3ab14250eaa7a7a969eea4b006cbd57f
Luis Henrique <101938473+Luis-Henrique@users.noreply.github.com>
2023-12-18 18:21:32 -0300

    refactor: custom openapi function (#54)
    * refactor: creating a new module for custom openapi function
    
    * test: update test custom openapi to a new file
    
    * refactor: update custom openapi from a class to a function
    
    * fix: function import

--- src/adapter.py
+++ src/adapter.py
@@ -1,10 +1,10 @@
 import re
-from typing import Any, Callable, Dict, Optional
+from typing import Any, Dict, Optional
 
 from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
 
 from cloudformation import CloudformationTemplate, NodeType
+from openapi import custom_openapi
 from routing import APIRoute
 
 ARN_PATTERN = r"^arn:aws:apigateway.*\${(\w+)\.Arn}/invocations$"
@@ -108,51 +108,3 @@ class SAM:
                     methods=[method],
                     route_class_override=APIRoute,
                 )
-
-
-def custom_openapi(app: FastAPI, openapi_schema: Dict[str, Any]) -> Callable[[], Dict[str, Any]]:
-    def openapi() -> Dict[str, Any]:
-        if app.openapi_schema is not None:
-            return app.openapi_schema
-
-        fastapi_schema = get_openapi(
-            title=app.title,
-            version=app.version,
-            summary=app.summary,
-            description=app.description,
-            routes=app.routes,
-        )
-
-        paths = {**fastapi_schema["paths"], **openapi_schema["paths"]}
-
-        openapi_components = openapi_schema.get("components", {})
-        openapi_schemas = openapi_components.get("schemas", {})
-        openapi_security_schemes = openapi_components.get("securitySchemes", {})
-        openapi_examples = openapi_components.get("examples", {})
-
-        fastapi_components = fastapi_schema.get("components", {})
-        fastapi_schemas = fastapi_components.get("schemas", {})
-        fastapi_security_schemes = fastapi_components.get("securitySchemes", {})
-        fastapi_examples = fastapi_components.get("examples", {})
-
-        schemas = {**fastapi_schemas, **openapi_schemas}
-        security_schemas = {**fastapi_security_schemes, **openapi_security_schemes}
-        examples = {**fastapi_examples, **openapi_examples}
-
-        components = {}
-
-        if schemas:
-            components["schemas"] = schemas
-
-        if security_schemas:
-            components["securitySchemes"] = security_schemas
-
-        if examples:
-            components["examples"] = examples
-
-        app.openapi_schema = {**openapi_schema, "paths": paths}
-        app.openapi_schema["components"] = components
-
-        return app.openapi_schema
-
-    return openapi
