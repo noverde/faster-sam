a27e10512e09ed2be332fe1d0864ab7c03ec89ff
Luis Henrique <henriqueluis3104@hotmail.com>
2024-01-03 10:33:13 -0300

    feat: add read the docs initial configuration

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = "Faster SAM"
+copyright = "2024, Dotz"
+author = "Dotz"
+release = "0.1.0"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
