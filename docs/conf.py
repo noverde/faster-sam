c0fd6eeb97ab3ffcd8821d06cc03151018e74794
Luis Henrique <henriqueluis3104@hotmail.com>
2024-01-04 12:37:18 -0300

    feat: update documentation configuration of class signature params

--- docs/conf.py
+++ docs/conf.py
@@ -9,3 +9,6 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "alabaster"
 html_static_path = ["_static"]
+
+python_maximum_signature_line_length = 10
+autodoc_typehints = "none"
