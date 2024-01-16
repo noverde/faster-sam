9c46c76718ec3dbc0f9231bc3d37af6a36d68d05
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2024-01-16 11:34:04 -0300

    feat: add initial configuration for middleware

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,9 +1,10 @@
+from http import HTTPStatus
 import json
 import unittest
 
 from fastapi import FastAPI, Request, Response
 
-from faster_sam.middleware import RemovePathMiddleware
+from faster_sam.middleware import LambdaAuthorizer, RemovePathMiddleware
 
 
 class TestRemovePathMiddleware(unittest.IsolatedAsyncioTestCase):
@@ -19,3 +20,36 @@ class TestRemovePathMiddleware(unittest.IsolatedAsyncioTestCase):
         response = await middleware.dispatch(request, call_next)
 
         self.assertEqual(json.loads(response.body), {"path": "/foo"})
+
+
+class TestLambdaAuthorizer(unittest.IsolatedAsyncioTestCase):
+    async def test_middleware_unauthorized(self):
+        app = FastAPI()
+
+        middleware = LambdaAuthorizer(
+            app, "arn:aws:lambda:region:account-id:function:function-name"
+        )
+
+        async def call_next(request: Request) -> Response:
+            pass
+
+        request = Request(
+            scope={
+                "type": "http",
+                "method": "GET",
+                "path": "/test/foo",
+                "headers": [
+                    (b"content-type", b"application/json"),
+                    (b"user-agent", b"python/unittest"),
+                    (
+                        b"Authorization",
+                        b"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJib3Jyb3dlcl9pZCI6NDM3MSwiY3Bm",
+                    ),
+                ],
+            }
+        )
+        response = await middleware.dispatch(request, call_next)
+        body = json.loads(response.body)
+
+        self.assertEqual(body["message"], "Unauthorized")
+        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED.value)
