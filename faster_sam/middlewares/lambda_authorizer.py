758aae2eb3001cdcdd52b0f2b7e8ebdf65367fac
Gabriel Figueiredo <53541827+gabriel-f-santos@users.noreply.github.com>
2024-01-23 18:48:53 -0300

    feat: add initial configuration for middleware (#81)
    * feat: add initial configuration for middleware
    
    * feat: pass client boto 3as dependency
    
    * refactor: sort imports
    
    * fix: optional client
    
    * refactor: improve doc strings
    
    * fix: add spaces docstring
    
    * fix: lambda and AWS format name, change return not invoked lambda
    
    * fix: fix test middleware authorization
    
    * feat: add authorize module in middlewares
    
    * refact: fix sorting imports
    
    * fix: add boto3
    
    * refactor: refactor lambda authorizer middleware
    
    * feat: add tests middleware auth
    
    * refactor: remove response metadata
    
    * refactor: improve argument name
    
    * feat: add mapping from request scope to aws event
    
    * refactor: separate middleware tests
    
    * fix: fix headers convert to dict
    
    * fix: fix erro exception lambda authorizer
    
    * fix: pass authorization as lower case as expected in request.scope
    
    * feat: add docs build_event
    
    * feat: add more info to event
    
    * fix: add more info to method arn in event api gateway
    
    * feat: add client class (#85)
    
    * feat: add client class
    
    * feat: update lambda authorizer
    
    * feat: add values to event
    
    * refactor: update lambda authorizer
    
    * refactor: change set and assume role methods
    
    * chore: add documentation to functions
    
    * chore: improve documentation
    
    * chore: improve attributes runtime
    
    * feat: add client lambda tests
    
    * feat: update class atributes
    
    * feat: update tests
    
    * chore: sort imports
    
    * feat: minor improvements
    
    * docs: update docstrings
    
    ---------
    
    Co-authored-by: Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
    Co-authored-by: Juliano Fernandes <julianofernandes@gmail.com>
    
    * chore: change optional-dependencies name
    
    * chore: remove version from packages dependencies
    
    ---------
    
    Co-authored-by: Luis Henrique <101938473+Luis-Henrique@users.noreply.github.com>
    Co-authored-by: Juliano Fernandes <julianofernandes@gmail.com>

---
+++ faster_sam/middlewares/lambda_authorizer.py
@@ -0,0 +1,313 @@
+import json
+import logging
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta, timezone
+from http import HTTPStatus
+from typing import Any, Callable, Dict, Optional
+from uuid import uuid4
+
+import boto3
+from botocore.client import BaseClient
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Credentials:
+    access_key: Optional[str] = field(default=None)
+    secret_access_key: Optional[str] = field(default=None)
+    session_token: Optional[str] = field(default=None)
+    role_arn: Optional[str] = field(default=None)
+    web_identity_token: Optional[str] = field(default=None)
+    web_identity_callable: Optional[Callable[[], Optional[str]]] = field(default=None)
+    role_session_name: Optional[str] = field(default=None)
+    region: Optional[str] = field(default=None)
+
+
+class LambdaClient:
+    """
+    AWS Lambda client for handling session lifecycle.
+
+    e.g
+
+    This example get a lambda client.
+
+    >>> credentials = Credentials()
+    >>> lambda_client = LambdaClient(credentials)
+    >>> lambda_client.client.invoke(FunctionName="my-lambda-function")
+
+    Parameters
+    ----------
+    credentials : Credentials
+        The AWS credentials used to authenticate with the Lambda service.
+    session_duration : int, optional
+        The duration (in seconds) for which the assumed role credentials should be valid.
+        Defaults to 900 seconds (15 minutes).
+    expiration_threshold : int, optional
+        The expiration threshold (in seconds) to check if the credentials are considered expired.
+        Defaults to 2 seconds.
+    """
+
+    def __init__(
+        self, credentials: Credentials, session_duration: int = 900, expiration_threshold: int = 2
+    ) -> None:
+        """
+        Initializes the LambdaClient.
+        """
+        self._credentials = credentials
+        self._session_duration = session_duration
+        self._expiration_threshold = timedelta(seconds=expiration_threshold)
+        self._expires_at = None
+        self._client = None
+
+    @property
+    def client(self) -> BaseClient:
+        """
+        Returns the AWS Lambda client instance.
+
+        Returns
+        -------
+        Client
+            The AWS Lambda client instance.
+        """
+        if self._client is None:
+            self.set_client()
+
+        if self.expired:
+            self.refresh()
+
+        return self._client  # type: ignore
+
+    @property
+    def expired(self) -> bool:
+        """
+        Checks if the client session has expired.
+
+        Returns
+        -------
+        bool
+            True if session has expired, False otherwise.
+        """
+        if self._expires_at is None:
+            return False
+
+        now = datetime.now(tz=timezone.utc)
+        remaining = self._expires_at - now
+
+        return remaining < self._expiration_threshold
+
+    def assume_role(self) -> Credentials:
+        """
+        Attempt to assume the role defined inside the credentials.
+
+        Returns
+        -------
+        Credentials
+            The assumed role temporary credentials.
+        """
+        sts = boto3.client("sts")
+
+        if callable(self._credentials.web_identity_callable):
+            function = sts.assume_role_with_web_identity
+            web_identity = self._credentials.web_identity_callable()
+        elif self._credentials.web_identity_token is not None:
+            function = sts.assume_role_with_web_identity
+            web_identity = self._credentials.web_identity_token
+        else:
+            raise NotImplementedError()
+
+        response = function(
+            DurationSeconds=self._session_duration,
+            RoleArn=self._credentials.role_arn,
+            RoleSessionName=self._credentials.role_session_name,
+            WebIdentityToken=web_identity,
+        )
+
+        expires = response["Credentials"]["Expiration"]
+        self._expires_at = expires.astimezone(tz=timezone.utc)
+
+        return Credentials(
+            access_key=response["Credentials"]["AccessKeyId"],
+            secret_access_key=response["Credentials"]["SecretAccessKey"],
+            session_token=response["Credentials"]["SessionToken"],
+            region=self._credentials.region,
+        )
+
+    def set_client(self) -> None:
+        """
+        Sets the AWS Lambda client with the provided credentials.
+        """
+        credentials = self._credentials
+
+        if self._credentials.role_arn is not None:
+            credentials = self.assume_role()
+
+        self._client = boto3.client(
+            "lambda",
+            aws_access_key_id=credentials.access_key,
+            aws_secret_access_key=credentials.secret_access_key,
+            aws_session_token=credentials.session_token,
+            region_name=credentials.region,
+        )
+
+    def refresh(self) -> None:
+        """
+        Refreshes the client session.
+        """
+        self.set_client()
+
+
+class LambdaAuthorizerMiddleware(BaseHTTPMiddleware):
+    """
+    Invoke Lambda function at AWS to authorize API requests.
+
+    e.g
+
+    This example apply the middleware to authorize an app.
+
+    >>> app = FastAPI()
+    >>> app.add_middleware(LambdaAuthorizer, lambda_name="arn:aws:lambda:region:id:function:name")
+
+    Parameters
+    ----------
+        app : ASGIApp
+            Application instance the middleware is being registered to.
+        lambda_function : str
+            The Lambda function name or its ARN.
+        client: BaseClient
+            Client Lambda object
+    """
+
+    def __init__(
+        self,
+        app: ASGIApp,
+        lambda_function: str,
+        credentials: Credentials = Credentials(),
+    ) -> None:
+        """
+        Initializes the LambdaAuthorizer.
+        """
+        super().__init__(app, self.dispatch)
+        self._lambda_function = lambda_function
+        self._lambda_client = None
+        self._credentials = credentials
+
+    @property
+    def client(self) -> BaseClient:
+        """
+        Returns the Lambda client instance.
+
+        Returns
+        -------
+        BaseClient
+            The Lambda client instance.
+        """
+        if self._lambda_client is None:
+            self._lambda_client = LambdaClient(self._credentials)
+
+        return self._lambda_client.client
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        """
+        Allow or deny a request based on authorization function rules.
+
+        Parameters
+        ----------
+        request : Request
+            The incoming request.
+        call_next : RequestResponseEndpoint
+            Next middleware or endpoint on the execution stack
+
+        Returns
+        -------
+        Response
+            The response generated by the middleware.
+        """
+
+        payload = self.invoke_lambda(request)
+
+        if payload and payload["policyDocument"]["Statement"][0]["Effect"] == "Allow":
+            request.scope["authorization_context"] = payload.get("context")
+            return await call_next(request)
+
+        content = {"message": "Unauthorized"}
+        status_code = HTTPStatus.UNAUTHORIZED
+
+        if payload is None:
+            content = {"message": "Something went wrong. Try again"}
+            status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+
+        return Response(content=json.dumps(content), status_code=status_code.value)
+
+    def invoke_lambda(self, request: Request) -> Optional[Dict[str, Any]]:
+        """
+        Invoke an AWS Lambda.
+
+        Parameters
+        ----------
+        request : Request
+            The incoming request.
+
+        Returns
+        -------
+        Response
+            The response payload generated by AWS Lambda invoked.
+        """
+        input_payload = self.build_event(request)
+
+        try:
+            response = self.client.invoke(
+                FunctionName=self._lambda_function, Payload=json.dumps(input_payload)
+            )
+        except Exception as error:
+            logger.exception(error)
+            return None
+
+        data = response["Payload"].read()
+
+        output_payload = json.loads(data)
+
+        return output_payload
+
+    def build_event(self, request: Request) -> Optional[Dict[str, Any]]:
+        """
+        Convert request object to an AWS API Gateway request authorizer event.
+
+        Parameters
+        ----------
+        request : Request
+            The incoming request.
+
+        Returns
+        -------
+        event: Dict
+            Event in AWS API Gateway format.
+        """
+        event = {
+            "type": "REQUEST",
+            "methodArn": f"arn:aws:execute-api:region:account-id:/{request.method}/{request.url.path}",  # noqa
+            "resource": request.url.path,
+            "path": request.url.path,
+            "httpMethod": request.method,
+            "headers": dict(request.headers),
+            "queryStringParameters": dict(request.query_params),
+            "pathParameters": request.path_params,
+            "requestContext": {
+                "path": request.url.path,
+                "stage": request.app.version,
+                "requestId": str(uuid4()),
+                "identity": {
+                    "userAgent": request.headers.get("user-agent"),
+                    "sourceIp": getattr(request.client, "host", None),
+                },
+                "httpMethod": request.method,
+                "domainName": request.url.hostname,
+                "apiId": request.scope.get("http_version"),
+                "accountId": "account-id",
+            },
+        }
+        return event
---
+++ tests/test_middleware_lambda_authorizer.py
@@ -0,0 +1,265 @@
+import io
+import json
+import unittest
+from datetime import datetime, timedelta, timezone
+from http import HTTPStatus
+from unittest import mock
+
+from botocore.exceptions import ClientError
+from botocore.response import StreamingBody
+from fastapi import FastAPI, Request, Response
+
+from faster_sam.middlewares import lambda_authorizer
+
+
+def invokation_response(effect: str):
+    response_payload = json.dumps(
+        {
+            "principalId": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ",
+            "policyDocument": {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Action": "execute-api:Invoke",
+                        "Effect": effect,
+                        "Resource": ["arn:aws:execute-api:us-east4:xpl3tuf2r0/v1/*/*"],
+                    }
+                ],
+            },
+            "context": {},
+        }
+    )
+
+    response = {
+        "StatusCode": 200,
+        "LogResult": "asdfg==",
+        "ExecutedVersion": "$LATEST",
+        "Payload": StreamingBody(
+            raw_stream=io.BytesIO(response_payload.encode()),
+            content_length=len(response_payload),
+        ),
+    }
+    return response
+
+
+class TestLambdaAuthorizerMiddleware(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.boto_patch = mock.patch("faster_sam.middlewares.lambda_authorizer.boto3")
+        self.mock_boto = self.boto_patch.start()
+
+        self.aws_response = {
+            "Credentials": {
+                "AccessKeyId": "154vc8sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "SecretAccessKey": "51fd5g4sdsdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "SessionToken": "sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "Expiration": datetime.now(tz=timezone.utc) + timedelta(minutes=2),
+            },
+            "SubjectFromWebIdentityToken": "225554488662322215444",
+            "AssumedRoleUser": {
+                "AssumedRoleId": "AROAYADG7OK3I5GGAIWYS:my-role-session-name",
+                "Arn": "arn:aws:sts::22555448866:assumed-role/role-to-assume/my-role-session-name",
+            },
+            "Provider": "accounts.google.com",
+            "Audience": "225554488662322215444",
+            "ResponseMetadata": {
+                "RequestId": "3412e653-78f1-4754-9a08-9446df59fefe",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "x-amzn-requestid": "3412e653-78f1-4754-9a08-9446df59fefe",
+                    "content-type": "text/xml",
+                    "content-length": "1425",
+                    "date": "Thu, 18 Jan 2024 14:20:16 GMT",
+                },
+                "RetryAttempts": 0,
+            },
+        }
+
+        self.mock_boto.client.return_value.assume_role_with_web_identity.return_value = (
+            self.aws_response
+        )
+
+        app = FastAPI()
+
+        credentials = lambda_authorizer.Credentials(
+            role_arn="arn:aws:iam::22555448866:role/role-to-assume",
+            web_identity_token="eyJhbGciOiJSUzI1Ni.eyJhdWQiOiJodHRwczov.b25hd3MuY29tIi",
+            role_session_name="my-role-session-name",
+        )
+
+        self.middleware = lambda_authorizer.LambdaAuthorizerMiddleware(
+            app, "arn:aws:lambda:region:account-id:function:function-name", credentials
+        )
+        self.scope = {
+            "type": "http",
+            "http_version": "1.1",
+            "method": "GET",
+            "root_path": "",
+            "query_string": [],
+            "path_params": {},
+            "path": "/test/foo",
+            "client": ("127.0.0.1", 80),
+            "app": FastAPI(),
+            "headers": [
+                (b"host", b"localhost:8000"),
+                (b"user-agent", b"curl/7.81.0"),
+                (b"accept", b"*/*"),
+                (b"authorization", b"eyJhbGciOiJIUzI1.eyJib3Jyb4Ik1TF9.JKvZfg5LZ9L96k"),
+            ],
+        }
+
+    def tearDown(self) -> None:
+        self.boto_patch.stop()
+
+    async def test_middleware_unauthorized(self):
+        request = Request(scope=self.scope)
+
+        mocked_invokation_response = invokation_response("Deny")
+        self.mock_boto.client.return_value.invoke.return_value = mocked_invokation_response
+
+        response = await self.middleware.dispatch(request, lambda x: ...)
+        body = json.loads(response.body)
+
+        self.assertEqual(body["message"], "Unauthorized")
+        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED.value)
+
+    async def test_middleware_authorized(self):
+        async def call_next(request: Request) -> Response:
+            return Response(
+                content=json.dumps({"message": "Authorized"}), status_code=HTTPStatus.OK.value
+            )
+
+        request = Request(scope=self.scope)
+
+        mocked_invokation_response = invokation_response("Allow")
+        self.mock_boto.client.return_value.invoke.return_value = mocked_invokation_response
+
+        response = await self.middleware.dispatch(request, call_next)
+        body = json.loads(response.body)
+
+        self.assertEqual(body["message"], "Authorized")
+        self.assertEqual(response.status_code, HTTPStatus.OK.value)
+
+    async def test_middleware_internal_server_error(self):
+        request = Request(scope=self.scope)
+        client_lambda = self.mock_boto.client.return_value
+
+        client_lambda.invoke.side_effect = ClientError(
+            error_response={
+                "Error": {
+                    "Code": "ServiceException",
+                    "Message": "An error occurred (ServiceException) when calling the InvokeFunction operation (reached max retries: 4): An error occurred and the request cannot be processed.",  # noqa
+                }
+            },
+            operation_name="InvokeFunction ",
+        )
+
+        response = await self.middleware.dispatch(request, lambda x: ...)
+        body = json.loads(response.body)
+        self.assertEqual(body["message"], "Something went wrong. Try again")
+        self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR.value)
+
+
+class TestLambdaClient(unittest.TestCase):
+    def setUp(self) -> None:
+        self.boto_patch = mock.patch("faster_sam.middlewares.lambda_authorizer.boto3")
+        self.mock_boto = self.boto_patch.start()
+
+        self.aws_response = {
+            "Credentials": {
+                "AccessKeyId": "154vc8sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "SecretAccessKey": "51fd5g4sdsdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "SessionToken": "sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+                "Expiration": datetime.now(tz=timezone.utc) + timedelta(minutes=2),
+            },
+            "SubjectFromWebIdentityToken": "225554488662322215444",
+            "AssumedRoleUser": {
+                "AssumedRoleId": "AROAYADG7OK3I5GGAIWYS:my-role-session-name",
+                "Arn": "arn:aws:sts::22555448866:assumed-role/role-to-assume/my-role-session-name",
+            },
+            "Provider": "accounts.google.com",
+            "Audience": "225554488662322215444",
+            "ResponseMetadata": {
+                "RequestId": "3412e653-78f1-4754-9a08-9446df59fefe",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "x-amzn-requestid": "3412e653-78f1-4754-9a08-9446df59fefe",
+                    "content-type": "text/xml",
+                    "content-length": "1425",
+                    "date": "Thu, 18 Jan 2024 14:20:16 GMT",
+                },
+                "RetryAttempts": 0,
+            },
+        }
+
+        self.mock_boto.client.return_value.assume_role_with_web_identity.return_value = (
+            self.aws_response
+        )
+
+        self.common_credentials = {
+            "role_arn": "arn:aws:iam::22555448866:role/role-to-assume",
+            "role_session_name": "my-role-session-name",
+            "region": "region1",
+        }
+
+        self.credentials_with_web_token = lambda_authorizer.Credentials(
+            web_identity_token="eyJhbGciOiJSUzI1Ni.eyJhdWQiOiJodHRwczov.b25hd3MuY29tIi",
+            **self.common_credentials,
+        )
+
+        def web_identity_callable():
+            return "eyJhbGciOiJSUzI1Ni.eyJhdWQiOiJodHRwczov.b25hd3MuY29tIi"
+
+        self.credentials_with_web_token_function = lambda_authorizer.Credentials(
+            web_identity_callable=web_identity_callable, **self.common_credentials
+        )
+
+        self.credentials_with_session_token = lambda_authorizer.Credentials(
+            access_key="154vc8sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+            secret_access_key="51fd5g4sdsdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+            session_token="sdffBG45W$#6f$56%W$W%V5$BWVE787Trdg",
+            region="region1",
+        )
+
+    def tearDown(self) -> None:
+        self.boto_patch.stop()
+
+    def initialize_lambda_client(self, credentials):
+        return lambda_authorizer.LambdaClient(credentials)
+
+    def test_assume_role(self):
+        web_token = {
+            "credentials_with_web_token": self.credentials_with_web_token,
+            "credentials_with_web_token_function": self.credentials_with_web_token_function,
+        }
+
+        for use_case, token_type in web_token.items():
+            with self.subTest(use_case=use_case):
+                client = self.initialize_lambda_client(token_type)
+                credentials = client.assume_role()
+
+                self.assertEqual(credentials, self.credentials_with_session_token)
+
+    def test_set_client(self):
+        client = self.initialize_lambda_client(self.credentials_with_web_token)
+        client.set_client()
+
+        self.mock_boto.client.assert_called_with(
+            "lambda",
+            aws_access_key_id=self.credentials_with_session_token.access_key,
+            aws_secret_access_key=self.credentials_with_session_token.secret_access_key,
+            aws_session_token=self.credentials_with_session_token.session_token,
+            region_name=self.credentials_with_web_token.region,
+        )
+
+    def test_expired_false(self):
+        client = self.initialize_lambda_client(self.credentials_with_web_token)
+        client.set_client()
+
+        self.assertEqual(client.expired, False)
+
+    def test_expired_true(self):
+        client = self.initialize_lambda_client(self.credentials_with_web_token)
+        client.set_client()
+        client._expires_at = client._expires_at - timedelta(minutes=2)
+
+        self.assertEqual(client.expired, True)
