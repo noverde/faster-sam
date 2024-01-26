3f48defbb5fa73adc15b00fd84949c4b30548f86
Luis Henrique <henriqueluis3104@hotmail.com>
2024-01-26 14:54:09 -0300

    docs: update docstrings

--- faster_sam/web_identity_providers.py
+++ faster_sam/web_identity_providers.py
@@ -27,7 +27,6 @@ class ProviderInterface(ABC):
     This abstract base class defines an interface for authentication providers
     to implement. Subclasses must implement the `get_token` method to provide
     functionality for retrieving authentication tokens.
-
     """
 
     @abstractmethod
@@ -38,11 +37,10 @@ class ProviderInterface(ABC):
         This method should be implemented by subclasses to retrieve the
         authentication token from the respective authentication provider.
 
-        Parameters
-        ----------
-        Returns:
-            token : str or None
-                The authentication token if available, else None.
+        Returns
+        -------
+        token : str or None
+            The authentication token if available, else None.
         """
         pass  # pragma: no cover
 
@@ -58,8 +56,8 @@ class GCPProvider(ProviderInterface):
     >>> provider = GCPProvider(audience="https://myaudience.com")
     >>> provider.get_token()
 
-    Parameters
-    ----------
+    Environment Variables
+    ---------------------
     audience : str
         The unique URI agreed upon by both the instance and the
         system verifying the instance's identity. For example, the audience
@@ -69,31 +67,17 @@ class GCPProvider(ProviderInterface):
         instance details are included in the payload. Specify `full` to
         include this information in the payload or standard to omit the
         information from the payload. The default value is `standard`.
-    licenses : str
+    licenses : bool
         An optional parameter that specifies whether license
         codes for images associated with this instance are included in the
         payload. Specify TRUE to include this information or FALSE to omit
         this information from the payload. The default value is FALSE.
         Has no effect unless format is `full`.
-
     """
 
     def __init__(self) -> None:
         """
         Initializes the GCPProvider.
-
-        Parameters
-        ----------
-        audience : str
-            The unique URI agreed upon by both the instance and the
-            system verifying the instance's identity.
-        format : str
-            The optional parameter that specifies whether the project and
-            instance details are included in the payload.
-        licenses : str
-            An optional parameter that specifies whether license
-            codes for images associated with this instance are included in the
-            payload.
         """
         audience = os.getenv("WEB_IDENTITY_AUDIENCE", "")
         format = os.getenv("WEB_IDENTITY_FORMAT", "standard")
@@ -149,8 +133,8 @@ def factory(provider: str) -> ProviderInterface:
     provider : str
         The name of the authentication provider.
 
-    Returns:
-    ----------
+    Returns
+    -------
     provider : ProviderInterface
         An instance of the authentication provider class
         corresponding to the specified provider name.
