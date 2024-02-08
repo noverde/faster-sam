cdd51d9921a6d1540c7c8a1582cddda684bbc91b
Gabriel Figueiredo <53541827+gabriel-f-santos@users.noreply.github.com>
2024-02-08 11:04:15 -0300

    Wip/redis  (#99)
    * wip: add redis cache
    
    * refactor: add cache to a new module
    
    * wip: enum authorization
    
    * refactor: change authorization funciton
    
    * refactor: move cache to invoke
    
    * fix: add redis to requirements-dev
    
    * fix: add ttl
    
    * refactor: remove key attr
    
    * refactor: update cache on lambda authorizer
    
    * feat: add tests to cache
    
    * fix: ttl type annotation
    
    * feat: add documentation to interface
    
    * feat: add documentation to RedisCache
    
    * feat: add documentation to methods get and set cache in function of authorization
    
    * refactor: update module name
    
    * fix: type annotations
    
    ---------
    
    Co-authored-by: Luis Henrique <henriqueluis3104@hotmail.com>
    Co-authored-by: Juliano Fernandes <julianofernandes@gmail.com>

---
+++ faster_sam/cache/redis_cache.py
@@ -0,0 +1,97 @@
+import os
+from typing import Optional
+from redis import Redis
+from faster_sam.cache.cache_interface import CacheInterface
+
+
+CACHE_TTL = int(os.getenv("FASTER_SAM_CACHE_TTL", 900))
+CACHE_URL = os.getenv("FASTER_SAM_CACHE_URL", "")
+
+
+class RedisCache(CacheInterface):
+    """
+    A cache backend implementation using Redis.
+
+    This class implements the CacheInterface and provides functionality
+    for setting and retrieving cached values using Redis as the underlying
+    cache storage.
+
+    e.g
+
+    Example of usage.
+
+    >>> cache = RedisCache()
+    >>> cache.set("my-key", "my-value")
+    >>> cache.get("my-key")
+
+    Environment Variables
+    ---------------------
+    FASTER_SAM_CACHE_TTL : int
+        The time-to-live (TTL) for the cached values. This
+        variable specifies the duration in seconds for which cached values will remain valid.
+        For example, setting this variable to 900 would make cached values expire after 15 minutes.
+
+    FASTER_SAM_CACHE_URL : str
+        The URL specifying the connection string that should contain the URL used
+        to establish a connection to the Redis server where the cache is hosted.
+        The URL format should follow the standard Redis connection string format like:
+        redis://127.0.0.1:6379/0
+    """
+
+    _connection: Optional[Redis] = None
+
+    @property
+    def connection(self) -> Redis:
+        """
+        Lazily initializes and returns the Redis connection object.
+
+        If the connection has not been initialized yet, it will be created
+        using the Redis URL specified in the environment variable `CACHE_URL`.
+
+        Returns
+        -------
+        Redis
+            The Redis connection object.
+        """
+        if self._connection is None:
+            self._connection = Redis.from_url(url=CACHE_URL)
+
+        return self._connection
+
+    def set(self, key: str, value: str, ttl: int = CACHE_TTL) -> None:
+        """
+        Set a value in the Redis cache.
+
+        Parameters
+        ----------
+        key : str
+            The key under which the value will be stored in the Redis cache.
+        value : str
+            The value to be stored in the Redis cache.
+        ttl : int, optional
+            The time-to-live (TTL) for the cached value in seconds.
+            The value is specified by `CACHE_TTL` constant with 900
+            seconds as default value if not provided.
+
+        Returns
+        -------
+        None
+        """
+        self.connection.set(key, value, ttl)
+
+    def get(self, key: str) -> Optional[str]:
+        """
+        Retrieve a value from the Redis cache.
+
+        Parameters
+        ----------
+        key : str
+            The key associated with the value to retrieve from the Redis cache.
+
+        Returns
+        -------
+        str or None
+            The value associated with the given key if it exists in the cache,
+            otherwise None.
+        """
+        return self.connection.get(key)
---
+++ tests/test_redis.py
@@ -0,0 +1,63 @@
+import unittest
+from unittest import mock
+from faster_sam.cache import redis_cache
+from faster_sam.cache.cache_interface import CacheInterface
+
+from faster_sam.cache.redis_cache import RedisCache
+
+
+class FakeRedis(CacheInterface):
+    def __init__(self):
+        self._db = {}
+
+    def set(self, key, value, ttl):
+        if isinstance(value, dict):
+            raise TypeError("A tuple item must be str, int, float or bytes.")
+        self._db[key] = {"value": value, "ttl": ttl}
+
+    def get(self, key):
+        return self._db.get(key, {}).get("value")
+
+    def flushdb(self):
+        self._db = {}
+
+
+class TestRedis(unittest.TestCase):
+    def setUp(self) -> None:
+        redis_cache.CACHE_URL = "redis://127.0.0.1:6379/0"
+
+        self.redis_patch = mock.patch("faster_sam.cache.redis_cache.Redis")
+        self.redis_mock = self.redis_patch.start()
+        self.redis_mock.from_url.return_value = FakeRedis()
+        self.key = "1234"
+
+    def tearDown(self) -> None:
+        RedisCache().connection.flushdb()
+        self.redis_patch.stop()
+
+    def test_get_connection(self):
+        cache = RedisCache()
+        connection = cache.connection
+
+        self.assertIsNotNone(connection)
+        self.assertIsInstance(connection, FakeRedis)
+
+    def test_cache(self):
+        cache = RedisCache()
+        cache.set("123", "teste")
+        payload = cache.get("123")
+
+        self.assertIsNotNone(payload)
+        self.assertEqual(payload, "teste")
+
+    def test_cache_not_exists(self):
+        cache = RedisCache()
+        payload = cache.get("1234")
+
+        self.assertIsNone(payload)
+
+    def test_cache_value_invalid_type(self):
+        cache = RedisCache()
+
+        with self.assertRaises(TypeError):
+            cache.set("123", {"key": "value"})
