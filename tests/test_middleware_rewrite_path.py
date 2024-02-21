ab9ed5efcab46740609d993996571867c8c8a1a3
Luis Henrique <henriqueluis3104@hotmail.com>
2024-02-21 12:50:39 -0300

    test: update middleware tests

--- tests/test_middleware_rewrite_path.py
+++ tests/test_middleware_rewrite_path.py
@@ -26,3 +26,37 @@ class TestRewritePathMiddleware(unittest.IsolatedAsyncioTestCase):
         response = await middleware.dispatch(request, call_next)
 
         self.assertEqual(json.loads(response.body), {"path": "/bar"})
+
+    async def test_middleware_rewrite_path_with_empty_body(self):
+        async def receive():
+            return {
+                "type": "http.request",
+                "body": b"",
+            }
+
+        app = FastAPI()
+
+        middleware = rewrite_path.RewritePathMiddleware(app)
+
+        async def call_next(request: Request) -> Response:
+            return Response(content=json.dumps({"path": request.scope["path"]}))
+
+        request = Request(scope={"type": "http", "method": "POST", "path": "/foo"}, receive=receive)
+
+        response = await middleware.dispatch(request, call_next)
+
+        self.assertEqual(json.loads(response.body), {"message": "Something went wrong. Try again"})
+
+    async def test_middleware_rewrite_path_without_post(self):
+        app = FastAPI()
+
+        middleware = rewrite_path.RewritePathMiddleware(app)
+
+        async def call_next(request: Request) -> Response:
+            return Response(content=json.dumps({"path": request.scope["path"]}))
+
+        request = Request(scope={"type": "http", "method": "GET", "path": "/foo"})
+
+        response = await middleware.dispatch(request, call_next)
+
+        self.assertEqual(json.loads(response.body), {"path": "/foo"})
