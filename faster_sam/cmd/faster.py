a27ffc6694f8307a8f7f66c1a5974cda26e15f59
Luis Henrique <henriqueluis3104@hotmail.com>
2024-04-03 12:19:11 -0300

    refactor: format

--- faster_sam/cmd/faster.py
+++ faster_sam/cmd/faster.py
@@ -14,7 +14,7 @@ def display(title: str, message: str) -> None:
 def output(format: Optional[str], value: Any) -> Any:
     if format is None:
         return str(value)
-    
+
     if format == "json":
         return json.dumps(value, indent=4)
 
@@ -34,14 +34,16 @@ def resources(args) -> None:
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Faster CLI tool.')
-    subparsers = parser.add_subparsers(title='subcommand', dest='subcommand', help='Subcommand to execute.')
-   
-    resources_parser = subparsers.add_parser('resources', help='Perform operations on resources.')
-    
-    resources_parser.add_argument('list', help='List resources.')
-    resources_parser.add_argument('--type', help='Specify the type of resource.')
-    resources_parser.add_argument('--output', help='Specify the output format.')    
+    parser = argparse.ArgumentParser(description="Faster CLI tool.")
+    subparsers = parser.add_subparsers(
+        title="subcommand", dest="subcommand", help="Subcommand to execute."
+    )
+
+    resources_parser = subparsers.add_parser("resources", help="Perform operations on resources.")
+
+    resources_parser.add_argument("list", help="List resources.")
+    resources_parser.add_argument("--type", help="Specify the type of resource.")
+    resources_parser.add_argument("--output", help="Specify the output format.")
 
     args = parser.parse_args()
 
