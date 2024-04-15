c8d33daef7d417ad3afa1f8c9d5f052b57893e0e
Luis Henrique <henriqueluis3104@hotmail.com>
2024-04-15 19:57:56 -0300

    refactor: update param type and variable name

--- faster_sam/cloudformation.py
+++ faster_sam/cloudformation.py
@@ -491,7 +491,7 @@ class IntrinsicFunctions:
         return None
 
     @staticmethod
-    def get_att(value: Union[List[Any], str], template: Dict[str, Any]) -> Optional[str]:
+    def get_att(value: Union[List[str], str], template: Dict[str, Any]) -> Optional[str]:
         """
         Gets the value of an attribute from a CloudFormation template based on a list
         of logical name and attribute name.
@@ -525,12 +525,12 @@ class IntrinsicFunctions:
         if attribute_name not in template["Resources"][logical_name]["Properties"]:
             return None
 
-        function_value = template["Resources"][logical_name]["Properties"][attribute_name]
+        attribute_value = template["Resources"][logical_name]["Properties"][attribute_name]
 
-        if isinstance(function_value, dict):
-            function_value = IntrinsicFunctions.eval(function_value, template)
+        if isinstance(attribute_value, dict):
+            attribute_value = IntrinsicFunctions.eval(attribute_value, template)
 
-            if function_value is None:
+            if attribute_value is None:
                 return None
 
-        return function_value
+        return attribute_value
