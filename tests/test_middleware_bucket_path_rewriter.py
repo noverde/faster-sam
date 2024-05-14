be4971c3ff988aa535afd04d13d4640a9c50066b
Gabriel Figueiredo <gabrielfgsantos95@gmail.com>
2024-05-14 19:29:12 -0300

    refactor: change rewrite path middleware for bucket

--- tests/test_middleware_bucket_path_rewriter.py
+++ tests/test_middleware_bucket_path_rewriter.py
@@ -2,61 +2,34 @@ import json
 import unittest
 
 from fastapi import FastAPI, Request, Response
+from fastapi.testclient import TestClient
 
 from faster_sam.middlewares import bucket_path_rewriter
 
 
-class TestBucketPathRewriterMiddleware(unittest.IsolatedAsyncioTestCase):
-    async def test_middleware_rewrite_path_with_bucket(self):
-        async def receive():
-            return {
-                "type": "http.request",
-                "body": b'{"bucket":"test-bucket-123"}',
-            }
+class TestBucketPathRewriterMiddleware(unittest.TestCase):
 
-        app = FastAPI()
-
-        middleware = bucket_path_rewriter.BucketPathRewriterMiddleware(app)
-
-        async def call_next(request: Request) -> Response:
+    def setUp(self) -> None:
+        async def bucket(request: Request) -> Response:
             return Response(content=json.dumps({"path": request.scope["path"]}))
 
-        request = Request(scope={"type": "http", "method": "POST", "path": "/"}, receive=receive)
+        app = FastAPI()
+        app.add_middleware(bucket_path_rewriter.BucketPathRewriterMiddleware)
+        app.add_route("/bucket", bucket)
 
-        response = await middleware.dispatch(request, call_next)
+        self.client = TestClient(app)
+
+    async def test_middleware_rewrite_path_with_bucket(self):
+        response = self.client.post("/bucket", json={"bucket": "test-bucket-123"})
 
         self.assertEqual(json.loads(response.body), {"path": "/test-bucket-123"})
 
     async def test_middleware_rewrite_path_with_empty_body(self):
-        async def receive():
-            return {
-                "type": "http.request",
-                "body": b"",
-            }
-
-        app = FastAPI()
-
-        middleware = bucket_path_rewriter.BucketPathRewriterMiddleware(app)
-
-        async def call_next(request: Request) -> Response:
-            return Response(content=json.dumps({"path": request.scope["path"]}))
-
-        request = Request(scope={"type": "http", "method": "POST", "path": "/"}, receive=receive)
-
-        response = await middleware.dispatch(request, call_next)
+        response = self.client.post("/bucket", json={})
 
         self.assertEqual(json.loads(response.body), {"message": "Invalid Request"})
 
     async def test_middleware_rewrite_path_without_post(self):
-        app = FastAPI()
-
-        middleware = bucket_path_rewriter.BucketPathRewriterMiddleware(app)
-
-        async def call_next(request: Request) -> Response:
-            return Response(content=json.dumps({"path": request.scope["path"]}))
-
-        request = Request(scope={"type": "http", "method": "GET", "path": "/"})
-
-        response = await middleware.dispatch(request, call_next)
+        response = self.client.get("/bucket")
 
         self.assertEqual(json.loads(response.body), {"path": "/"})
